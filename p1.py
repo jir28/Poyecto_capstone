04c898a32266d64fea4a1326429c5850ae9f3a50
Jair Zamorano <87546129+jir28@users.noreply.github.com>
2021-12-26 19:56:38 -0600

    Update p1.py
    Sensor calibrado y probado con agua, librerías del foco Xiaomi y sensor de temperatura

--- p1.py
+++ p1.py
@@ -1,5 +1,10 @@
-import RPi.GPIO as GPIO
-import time, sys
+
+import RPi.GPIO as GPIO #puertos de raspberry
+import time
+#from w1thermsensor import W1ThermSensor #libreria para sensor de temperatura de agua
+from yeelight import Bulb#Librerias para foco MI
+from yeelight import LightType
+
 f = open('flowmeter.txt','a')
 
 GPIO.setmode(GPIO.BCM)
@@ -8,7 +13,7 @@ led = 16
 GPIO.setup(inpt,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 minutos = 0
-constant = 0.006
+constant = 0.00210
 time_new = 0.0
 rpt_int = 10
 
@@ -22,23 +27,19 @@ def Pulse_cnt(inpt_pin):
     rate_cnt +=1
     tot_cnt +=1
     
-GPIO.add_event_detect(inpt,GPIO.FALLING,callback=Pulse_cnt,bouncetime=10)
+GPIO.add_event_detect(inpt,GPIO.FALLING,callback=Pulse_cnt)
 
 #MAIN
-print("Aproximacion de flujo de agua",str(time.asctime(time.localtime(time.time()))))
-rpt_int = int(input("Segundos de captar el flujo "))
+print("Registro de litros gastados y temperatura promedio el dia: ",str(time.asctime(time.localtime(time.time()))))
+rpt_int = int(input("\nSegundos de captar el flujo "))
 print("Reportes cada" ,rpt_int," segundos")
 print("Control + c para salir")
-f.write('\nFlujo de agua - Aproximado - Reportando cada '+str(rpt_int)+' segundos'+str(time.asctime(time.localtime(time.time()))))
+f.write('Registro de litros gastados y temperatura promedio el dia: '+str(time.asctime(time.localtime(time.time()))))
 inicio = time.time()
-while(rpt_int < 10 or GPIO.input(inpt)==True):#rpt_int<10 or <-- esta linea para pruebas antes de todo lo demas
+while(rpt_int < 20):#rpt_int<10 or,,or GPIO.input(inpt)==True <-- esta linea para pruebas antes de todo lo demas
     rpt_int+=1
-    time_new = time.time
-    rate_cnt = 0
-    minutos+=1
-    LperM = round(((rate_cnt*constant)/(rpt_int/60)),2)
     TotLit = round(tot_cnt*constant,1)
-    if(TotLit>=0.2 and aux==0):
+    if(TotLit>=1 and aux==0):
         GPIO.output(led, GPIO.HIGH)
         print("Excediste de litros")
         aux = 1
@@ -47,15 +48,17 @@ while(rpt_int < 10 or GPIO.input(inpt)==True):#rpt_int<10 or <-- esta linea para
 
 final=time.time()
 tiempo = round((final-inicio),2)
-print('Litros totales ',TotLit)
-print('Tiempo  ',tiempo)
-#f.write('\n Litros/minutos'+str(LperM))
-f.write(' Total de litros: '+str(TotLit))
-f.write(' Tiempo '+str(tiempo))
+print('Litros gastados: ',TotLit)
+print('Tiempo:  ',tiempo,' segundos')
+
+f.write('\nLitros gastados: '+str(TotLit))
+f.write('\nTiempo: '+str(tiempo))
 f.flush()
+
 if(GPIO.input(led)==True):
     GPIO.output(led,GPIO.LOW)
 
 GPIO.cleanup()
 f.close()
 print("Se acabo")
+    
