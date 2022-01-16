9e466586ef0118e920b3f5412b42dd17b9c940f2
Jair Zamorano <87546129+jir28@users.noreply.github.com>
2022-01-16 16:11:48 -0600

    Update p1.py

--- p1.py
+++ p1.py
@@ -2,16 +2,14 @@
 import RPi.GPIO as GPIO #puertos de raspberry
 import time
 #from w1thermsensor import W1ThermSensor #libreria para sensor de temperatura de agua
-from yeelight import Bulb#Librerias para foco MI
-from yeelight import LightType
+from yeelight import Bulb#Libreria para foco MI
 
 f = open('flowmeter.txt','a')
+bulb = Bulb("192.168.3.28")
 
 GPIO.setmode(GPIO.BCM)
 inpt = 13
-led = 16
 GPIO.setup(inpt,GPIO.IN)
-GPIO.setup(led,GPIO.OUT)
 minutos = 0
 constant = 0.00210
 time_new = 0.0
@@ -35,16 +33,21 @@ rpt_int = int(input("\nSegundos de captar el flujo "))
 print("Reportes cada" ,rpt_int," segundos")
 print("Control + c para salir")
 f.write('Registro de litros gastados y temperatura promedio el dia: '+str(time.asctime(time.localtime(time.time()))))
+bulb.turn_on()#encendemos el foco
+bulb.set_hsv(360, 0, 1)
+bulb.set_brightness(100)
+bulb.set_color_temp(6500)
 inicio = time.time()
 while(rpt_int < 20):#rpt_int<10 or,,or GPIO.input(inpt)==True <-- esta linea para pruebas antes de todo lo demas
     rpt_int+=1
     TotLit = round(tot_cnt*constant,1)
-    if(TotLit>=1 and aux==0):
-        GPIO.output(led, GPIO.HIGH)
+    if(TotLit>=2 and aux==0):
+        bulb.set_rgb(243 ,159, 24)
+        bulb.set_brightness(100)
+        bulb.set_color_temp(6500)#color rojo al foco
         print("Excediste de litros")
         aux = 1
-    time.sleep(1)
-
+    time.sleep(0.9)
 
 final=time.time()
 tiempo = round((final-inicio),2)
@@ -55,10 +58,8 @@ f.write('\nLitros gastados: '+str(TotLit))
 f.write('\nTiempo: '+str(tiempo))
 f.flush()
 
-if(GPIO.input(led)==True):
-    GPIO.output(led,GPIO.LOW)
-
 GPIO.cleanup()
 f.close()
+bulb.turn_off()
 print("Se acabo")
     
